fa08f5286bde7317b53fa35ca566253356b34431
Igor Polousov <igorpolousov@gmail.com>
2023-02-28 17:18:17 +0300

    + add ActivityRowView

---
+++ SwiftUI+CoreData/Views/ActivityViews/ActivityRowView.swift
@@ -0,0 +1,49 @@
+//
+//  ActivityView.swift
+//  SwiftUI+CoreData
+//
+//  Created by Igor Polousov on 28.02.2023.
+//
+
+import SwiftUI
+
+struct ActivityView: View {
+    
+    @State var title: String = "Label"
+    @State var subtitle: String = "SubtitleLabel"
+    
+    var body: some View {
+        
+        ZStack {
+            
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color(Theme.accentColor!))
+                .frame(height: 65)
+            
+            HStack {
+                Image("hotel")
+                    .foregroundColor(Color(Theme.tintColor!))
+                    .shadow(color:.white ,radius: 1.5)
+                
+                VStack(alignment:.leading) {
+                    Text(title)
+                        .foregroundColor(Color(.lightGray))
+                        .font(Font(Theme.activityFont!))
+                    Text(subtitle)
+                        .foregroundColor(Color(.lightGray))
+                        .font(Font(Theme.activityFont!))
+                }
+                .padding(.leading, 20)
+                
+                Spacer()
+            }
+            .padding(.leading, 10)
+        }
+    }
+}
+
+struct ActivityView_Previews: PreviewProvider {
+    static var previews: some View {
+        ActivityView()
+    }
+}
